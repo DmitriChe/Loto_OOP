74770deac948ddbb458c7cbc9277a4d76e515d99
DmitriChe <juneority@mail.ru>
2020-01-09 20:28:41 +0300

    Created magic methods __str__, __len__, __contains__, __item__, __eq__ && tests for them in class Bag

--- loto_classes.py
+++ loto_classes.py
@@ -14,6 +14,18 @@ class Bag:
     def stats(self):
         print(f'В мешке {len(self.nums)} боченков.')
 
+    def __str__(self):
+        str_nums = ''
+        for n in sorted(self.nums.copy()):
+            str_nums += f'{n}, '
+        return f'Всего {len(self.nums)} бочонков №№: {str_nums}'
+
+    def __eq__(self, other):
+        return sorted(self.nums) == sorted(other.nums)
+
+    def __len__(self):
+        return len(self.nums)
+
 
 class Card:
 
@@ -50,6 +62,26 @@ class Card:
             print() if i + 1 in (9, 18, 27) else print(' ', end='')
         print('-' * 26, end='\n')
 
+    def __str__(self):
+        str_card = f'\n-{self.player_name}{"-" * (26 - len(self.player_name) - 1)}\n'
+        for i in range(len(self.card)):
+            if self.card[i] is None:
+                str_card += '--'
+            elif self.card[i] == 0:
+                str_card += '  '
+            elif self.card[i] < 10:
+                str_card += f' {self.card[i]}'
+            else:
+                str_card += f'{self.card[i]}'
+            str_card += '\n' if i + 1 in (9, 18, 27) else ' '
+        str_card += '-' * 26 + '\n'
+        return str_card
+
+    def __eq__(self, other):
+        return sorted(self.card_nums) == sorted(other.card_nums)
+
+    def __len__(self):
+        return len(self.card_nums)
 
 class Computer:
 
@@ -68,6 +100,22 @@ class Computer:
 
     def stats(self):
         print(f'{self.name}. Осталось {len(self.nums)} чисел : {self.nums}')
+
+    def __str__(self):
+        str_card = str(self.card)
+        return str_card + f'Имя игрока: {self.name}\nОсталось {len(self.nums)} чисел : {sorted(self.nums)}'
+
+    def __eq__(self, other):
+        return (sorted(self.nums) == sorted(other.nums)) and (self.is_winner == other.is_winner)
+
+    def __contains__(self, item):
+        return True if self.name == item else False
+
+    def __getitem__(self, item):
+        return self.nums[item]
+
+    def __len__(self):
+        return len(self.nums)
 
 
 class User(Computer):
@@ -92,6 +140,16 @@ class User(Computer):
         elif answer == 'n':
             if num in self.card.card_nums:
                 self.is_looser = True
+
+    def __str__(self):
+        str_card = str(self.card)
+        return str_card + f'Имя игрока: {self.name}\nОсталось {len(self.nums)} чисел : {sorted(self.nums)}'
+
+    def __eq__(self, other):
+        return (sorted(self.nums) == sorted(other.nums)) and (self.is_winner == other.is_winner)
+
+    def __contains__(self, item):
+        return True if self.name == item else False
 
 
 class Game:
@@ -154,7 +212,7 @@ class Game:
         # удаляем лузеров
         for loser in self.losers:
             self.players.pop(loser.name)
-        # даляем список лузеров, т.к. мы их только что удалили
+        # удаляем список лузеров, т.к. мы их только что удалили
         self.losers = []
 
     def cards_show(self):
@@ -181,6 +239,148 @@ class Game:
 
         return False
 
+    def __str__(self):
+        str_resume = f'\nПАРАМЕТРЫ ИГРЫ:\nЧисло игроков: {self.num_users + self.num_compics}\n' \
+                    f'- биологических: {self.num_users}\n- кремниевых: {self.num_compics}\n' \
+                    f'Число боченков: {len(self.bag.nums)}\nНомеров в каждой карточке: {15}\n' \
+                    f'Чило победителей: {len(self.winners)}\nИмена победителей: {self.winners}\n' \
+                    f'Чило проигравших: {len(self.losers)}\nИмена проигравших: {self.losers}\n'
+
+        return str_resume
+
+    def __eq__(self, other):
+        return (self.num_users == other.num_users) \
+               and (self.num_compics == other.num_compics) \
+               and (self.winners == other.winners) \
+               and (self.losers == other.losers) \
+               and (self.bag == other.bag) \
+               and (self.players == other.players)
+
+    def __len__(self):
+        return len(self.players)
+
 
 if __name__ == '__main__':
-    pass
+    import copy
+
+    print('\nРаспечатываем объект класса Bag')
+    bag = Bag()
+    print(bag)
+
+    print('\nРаспечатываем объект класса Card')
+    card = Card('Ivan')
+    print(card)
+
+    print('\nРаспечатываем объект класса Computer')
+    comp = Computer('Terminator')
+    print(comp)
+
+    print('\nРаспечатываем объект класса User')
+    usr = User('Ванечка')
+    print((usr))
+
+    print('\nРаспечатываем объект класса Game')
+    game = Game()
+    print(game)
+
+    # Проверка сравнения объектов класса Bag
+    print('\nПроверка сравнения объектов класса Bag')
+
+    bag1 = Bag()
+    bag2 = Bag()
+    print(bag1 == bag2)
+
+    num3 = bag2.nums[3]
+    bag2.nums[3] = 99
+    print(bag1 == bag2)
+
+    bag2.nums[3] = num3
+    print(bag1 == bag2)
+
+    # Проверка сравнения объектов класса Card
+    print('\nПроверка сравнения объектов класса Card')
+
+    card1 = Card('card1')
+    card2 = Card('card1')
+    card3 = copy.copy(card1)
+
+    print(card1 == card2)
+    print(card2 == card3)
+    print(card3 == card1)
+
+    # Проверка сравнения объектов класса Computer и User
+    print('\nПроверка сравнения объектов класса Computer  и User')
+    cmp1 = Computer('comp')
+    cmp2 = Computer('comp')
+    cmp3 = copy.copy(cmp1)
+
+    usr1 = User('user')
+    usr2 = User('user')
+    usr3 = copy.copy(usr1)
+
+    print(cmp1 == cmp2)
+    print(cmp2 == cmp3)
+    print(cmp3 == cmp1)
+
+    print(usr1 == usr2)
+    print(usr2 == usr3)
+    print(usr3 == usr1)
+
+    print(usr1 == cmp3)
+
+    # Проверка сравнения объектов класса Game
+    print('\nПроверка сравнения объектов класса Game')
+
+    gam1 = Game()
+    gam2 = Game()
+    gam3 = copy.copy(gam1)
+
+    print(gam1 == gam2)
+    print(gam2 == gam3)
+    print(gam3 == gam1)
+
+    gam2.num_users = 6
+
+    print(gam1 == gam2)
+    print(gam2 == gam3)
+    print(gam3 == gam1)
+
+    # Проверка наличия игрока с данным именем в объекте класса User
+    print('\nПроверка наличия игрока с данным именем в объекте класса User')
+
+    user1 = User('Маняша')
+    user2 = User('Ванюша')
+    print('Маняша' in user1)
+    print('Маняша' in user2)
+
+    # Проверка наличия игрока с данным именем в объекте класса Computer
+    print('\nПроверка наличия игрока с данным именем в объекте класса Computer')
+
+    comp = Computer()
+    print('Computer' in comp)
+
+    # Проверка итерируемости списка незачеркнутых номеров в карточках игроков Computer и User
+    print('\nПроверка итерируемости списка незачеркнутых номеров в карточках игроков Computer и User')
+
+    usr = User()
+    for num in usr:
+        print(num)
+
+    i = 5
+    print(f'{i}-м числом на карточке {usr.name} является число {usr[i - 1]}')
+
+
+    # Проверка длины объектов классов Bag, Card, Computer, User, Game
+    print('\nПроверка длины объектов классов Bag, Card, Computer, User, Game')
+
+    bag = Bag()
+    card = Card('Игрок2')
+    comp = Computer()
+    user = User()
+    game = Game()
+
+    print(f'Длина объекта класса Bag = {len(bag)} боченков')
+    print(f'Длина объекта класса Card = {len(card)} незачеркнутых чисел')
+    print(f'Длина объекта класса Computer = {len(comp)} незачеркнутых чисел')
+    print(f'Длина объекта класса User = {len(user)} незачеркнутых чисел')
+    print(f'Длина объекта класса Game = {len(game)} игроков')
--- test_loto_classes.py
+++ test_loto_classes.py
@@ -15,6 +15,37 @@ class TestBag:
         bag = Bag()
         assert bag.nums[-1] == bag.next()
 
+    def test_bag_str(self):
+        bag = Bag()
+        assert str(bag) != ''
+        assert str(bag) == 'Всего 90 бочонков №№: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,' \
+                           ' 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,' \
+                           ' 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,' \
+                           ' 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69,' \
+                           ' 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, '
+
+    def test_bag_eq(self):
+        bag_one = Bag()
+        bag_two = Bag()
+        bag_three = Bag()
+        bag_three.nums[0] = 100
+        assert bag_one == bag_two
+        assert bag_one != bag_three
+
+    def test_bag_item(self):
+        bag = Bag()
+        bag.nums = sorted(bag.nums)
+        assert bag.nums[0] == 1
+        assert bag.nums[89] == 90
+        assert bag.nums[44] == bag.nums[89] // 2
+        assert bag.nums[0] != 0
+        assert bag.nums[89] != 89
+
+    def test_bag_len(self):
+        bag = Bag()
+        assert len(bag) == 90
+        assert len(bag) == len(bag.nums)
+
 
 class TestCard:
 
